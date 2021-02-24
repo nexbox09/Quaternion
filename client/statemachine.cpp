4f85590107adcd1aaa96e0b8ed60aadc404944a3
nexbox09 <68700670+nexbox09@users.noreply.github.com>
2021-02-23 23:34:31 -0600

    ejercicio en clase

--- client/statemachine.cpp
+++ client/statemachine.cpp
@@ -1,1 +1,29 @@
 #include "statemachine.h"
+#include <iostream>
+
+void StateMachine::imprimirEstado() {
+
+
+
+    std::cout << "El estado es" << _estado << "\n\n" ;
+} 
+
+void StateMachine::nuevoMensaje(std::string mensaje) {
+
+
+    switch(_estado) {
+
+        case Nombre:
+            _estado = Edad;
+            break;
+        case Edad:
+            _estado = Color;
+            break;
+        case Color:
+            _estado = Nombre;
+            break;
+        default:
+        std::cout << "Error, mal programador \n\n";
+    }
+            
+}
